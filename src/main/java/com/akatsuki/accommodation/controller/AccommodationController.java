125632b91f8c63e0acdaf264ce33afdbec20c8e1
Aleksandar Buljevic <abuljevic8@gmail.com>
2023-05-30 21:46:35 +0200

    Added CRUD for prices

--- src/main/java/com/akatsuki/accommodation/controller/AccommodationController.java
+++ src/main/java/com/akatsuki/accommodation/controller/AccommodationController.java
@@ -1,11 +1,15 @@
 package com.akatsuki.accommodation.controller;
 
 import com.akatsuki.accommodation.dto.AccommodationDto;
+import com.akatsuki.accommodation.dto.CustomPriceDto;
+import com.akatsuki.accommodation.model.Accommodation;
 import com.akatsuki.accommodation.service.AccommodationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/accommodation")
@@ -14,9 +18,32 @@ public class AccommodationController {
 
     private final AccommodationService accommodationService;
 
+    @GetMapping
+    public List<Accommodation> findAllAccommodations() {
+        return accommodationService.findAllAccommodations();
+    }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public void createAccommodation(@Valid @RequestBody AccommodationDto accommodationDto) {
         accommodationService.createAccommodation(accommodationDto);
     }
+
+    @PutMapping("/{id}/default-price/{price}")
+    public void updateDefaultPrice(@PathVariable Long id, @PathVariable int price) {
+        accommodationService.updateDefaultPrice(id, price);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/custom-price")
+    public void addCustomPrice(@Valid @RequestBody CustomPriceDto customPriceDto) {
+        accommodationService.addCustomPrice(customPriceDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/custom-price/{id}")
+    public void deleteCustomPrice(@PathVariable Long id) {
+        accommodationService.deleteCustomPrice(id);
+    }
+
 }
--- src/main/java/com/akatsuki/accommodation/model/Accommodation.java
+++ src/main/java/com/akatsuki/accommodation/model/Accommodation.java
@@ -1,5 +1,6 @@
 package com.akatsuki.accommodation.model;
 
+import com.akatsuki.accommodation.enums.PriceType;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,8 +22,13 @@ public class Accommodation {
     private String name;
     private String location;
     private List<String> photographs;
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Benefits benefits;
     private int minQuests;
     private int maxQuests;
+    @Enumerated(EnumType.STRING)
+    private PriceType priceType;
+    private int defaultPrice;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<CustomPrice> customPrices;
 }
