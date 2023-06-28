124ba7228b1631b4d6d31916f3e909a1707ee622
JSimic <JovanSimic@pvh.com>
2023-06-28 21:13:15 +0200

    Small corrections.

--- src/main/java/com/akatsuki/accommodation/feignclients/ReservationFeignClient.java
+++ src/main/java/com/akatsuki/accommodation/feignclients/ReservationFeignClient.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.time.LocalDate;
-
 @FeignClient(url = "${core.services.reservation-url}", value = "reservation-feign-client")
 public interface ReservationFeignClient {
 
     @GetMapping("/check-reservations-of-accommodation")
-    boolean checkReservationsOfAccommodation(@RequestParam(name = "id", required = true) Long id,
-                                             @RequestParam(name = "startDate", required = true) LocalDate startDate,
-                                             @RequestParam(name = "endDate", required = true) LocalDate endDate, @RequestHeader("Authorization") final String token);
+    boolean checkReservationsOfAccommodation(@RequestParam(name = "id") Long id,
+                                             @RequestParam(name = "startDate") String startDate,
+                                             @RequestParam(name = "endDate") String endDate, @RequestHeader("Authorization") final String token);
 }
 
